639701f169f0ec2434b36ad306bc2f01ef735972
prachi1292 <prachi2013srivastava@gmail.com>
2018-11-22 11:05:48 +0530

    second commit

---
+++ unica/footer.php
@@ -0,0 +1,56 @@
+<footer class="footer-section">
+   <div class="container footer-top">
+      <div class="row">
+         <!-- widget -->
+         <div class="col-sm-6 col-lg-5 footer-widget">
+            <div class="about-widget">
+               <img src="img/logo-light.png" alt="">
+               <p>We’re LiberatedIT, a well settled startup company based in New Delhi.Founded by Kunal Gupta in 2017 with a dream to be the best service provider in technical and business field,our brand promise is simple: to provide powerful IT solutions to small, medium and overall businesses that are looking to build success online.</p>
+               <div class="social pt-1">
+                  <a href=""><i class="fa fa-twitter-square"></i></a>
+                  <a href=""><i class="fa fa-facebook-square"></i></a>
+                  <a href=""><i class="fa fa-google-plus-square"></i></a>
+                  <a href=""><i class="fa fa-linkedin-square"></i></a>
+                  <a href=""><i class="fa fa-rss-square"></i></a>
+               </div>
+            </div>
+         </div>
+         <!-- widget -->
+         <div class="col-sm-6 col-lg-3 footer-widget">
+            <h6 class="fw-title">USEFUL LINK</h6>
+            <div class="dobule-link">
+               <ul>
+                  <li><a href="">Home</a></li>
+                  <li><a href="">About us</a></li>
+               </ul>
+                    <ul>
+                  <li><a href="">Home</a></li>
+                  <li><a href="">About us</a></li>
+               </ul>
+            </div>
+         </div>
+         <div class="col-sm-6 col-lg-4 footer-widget">
+            <h6 class="fw-title">CONTACT</h6>
+            <ul class="contact">
+               <li>
+                  <p><i class="fa fa-map-marker"></i>B-68(A) kalkaji,New Delhi</p>
+               </li>
+               <li>
+                  <p><i class="fa fa-phone"></i>+9111 40073228</p>
+               </li>
+               <li>
+                  <p><i class="fa fa-envelope"></i>info@liberatedit.com</p>
+               </li>
+            </ul>
+         </div>
+      </div>
+   </div>
+   <!-- copyright -->
+   <div class="copyright">
+      <div class="container">
+         <p>
+            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This is developed<i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">LiberatedIt</a>
+         </p>
+      </div>
+   </div>
+</footer>
---
+++ unica/header_top.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <title>Admit Card</title>
+   <meta charset="UTF-8">
+   <meta name="description" content="Unica University Template">
+   <meta name="keywords" content="event, unica, creative, html">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <!-- Favicon -->   
+   <link href="img/favicon.ico" rel="shortcut icon"/>
+   <!-- Google Fonts -->
+   <link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i" rel="stylesheet">
+   <!-- Stylesheets -->
+   <link rel="stylesheet" href="css/bootstrap.min.css"/>
+   <link rel="stylesheet" href="css/font-awesome.min.css"/>
+   <link rel="stylesheet" href="css/themify-icons.css"/>
+   <link rel="stylesheet" href="css/magnific-popup.css"/>
+   <link rel="stylesheet" href="css/animate.css"/>
+   <link rel="stylesheet" href="css/owl.carousel.css"/>
+   <link rel="stylesheet" href="css/style.css"/>
+</head>
+</html>
